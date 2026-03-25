60484b4754baaa419134fb1cc8a63f15978314fd
minthantko <minthantko101@gmail.com>
2026-03-26 00:23:20 +0700

    fix pickle file cant find error. move bm25 class and idendity  method from build_index.py to search_engine.py

--- search_engine.py
+++ search_engine.py
@@ -1,7 +1,37 @@
 import pickle
 import pandas as pd
 import numpy as np
+from scipy import sparse
+from sklearn.feature_extraction.text import TfidfVectorizer
 import config
+
+# BM25 is moved here or else 
+# AttributeError: Can't get attribute 'BM25' on <module '__main__' 
+# from '/Users/minthantko/Documents/cmu/cmu3_2/ir/food_bookmarking_recommendation_project/se481-backend/app.py'>
+class BM25(object):
+    def __init__(self, vectorizer, b=config.BM25_B, k1=config.BM25_K1):
+        self.vectorizer = vectorizer
+        self.b = b
+        self.k1 = k1
+
+    def fit(self, X):
+        self.vectorizer.fit(X)
+        self.y = super(TfidfVectorizer, self.vectorizer).transform(X)
+        self.avdl = self.y.sum(1).mean()
+
+    def transform(self, q):
+        b, k1, avdl = self.b, self.k1, self.avdl
+        len_y = self.y.sum(1).A1
+        q, = super(TfidfVectorizer, self.vectorizer).transform([q])
+        assert sparse.isspmatrix_csr(q)
+        y = self.y.tocsc()[:, q.indices]
+        denom = y + (k1 * (1 - b + b * len_y / avdl))[:, None]
+        idf = self.vectorizer._tfidf.idf_[None, q.indices] - 1.
+        numer = y.multiply(np.broadcast_to(idf, y.shape)) * (k1 + 1)
+        return (numer / denom).sum(1).A1
+    
+def identity(x):
+    return x
 
 bm25 = None
 df_recipes = None
